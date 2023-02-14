200078119e67f4c07c211d684a05f0e2d6543ad7
marian <marian.muller@serli.com>
2023-02-15 00:47:52 +0100

    working joybus + dummy button inputs

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,54 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <pico/cyw43_arch.h>
+#include <pico/multicore.h>
 
 #include "N64Console.hpp"
 #include "n64_definitions.h"
 
-N64Console *console;
+#define JOYBUS_PIN 0
+
+n64_report_t report = default_n64_report;
 uint32_t ms_since_boot = 0;
 uint32_t ms_last_switch = 0;
-bool led = false;
+
+void core1_entry() {
+    printf("[core1] Initializing joybus on core 1\n");
+    N64Console *console = new N64Console(JOYBUS_PIN, pio0);
+    // Handle poll commands
+    while(true) {
+        //printf("[core1] Waiting for POLL command from console...\n");
+        console->WaitForPoll();
+        // TODO Handle concurrent access to report!
+        // TODO use a fifo to transfer report between cores?
+        //printf("[core1] sending report\n");
+        console->SendReport(&report);
+    }
+}
 
 int main(void) {
+    set_sys_clock_khz(130'000, true);
+
     stdio_init_all();
-    printf("Starting pico-ntroller64\n");
+    printf("[core0] Starting pico-ntroller64\n");
     
     if (cyw43_arch_init()) {
-        printf("cyw43 init failed\n");
+        printf("[core0] cyw43 init failed\n");
         return -1;
     }
 
-    printf("Starting loop\n");
+    multicore_launch_core1(core1_entry);
+
+    printf("[core0] Starting loop\n");
     while(1) {
-        // TODO Change state of start/A buttons every 500ms
+        // Change state of start/A buttons every 500ms
         ms_since_boot = to_ms_since_boot(get_absolute_time());
         if ((ms_since_boot - ms_last_switch) >= 500) {
-            printf("Switching buttons\n");
+            printf("[core0] Switching buttons\n");
             ms_last_switch = ms_since_boot;
-            //report.start = !report.start;
-            //report.a = !report.a;
-            led = !led;
-            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led);
+            report.start = !report.start;
+            report.a = !report.a;
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, report.start);
         }
     }
 }
